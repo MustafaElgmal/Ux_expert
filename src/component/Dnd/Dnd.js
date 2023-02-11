e29fa67fd81b99b0eb9ba432adf900412fc6ecf4
youssef-mo7ey <100730409+youssef-mo7ey@users.noreply.github.com>
2023-02-11 13:50:18 +0200

    phase 2 enhancements
    phase 2 enhancements

--- src/component/Dnd/Dnd.js
+++ src/component/Dnd/Dnd.js
@@ -3,9 +3,10 @@ import {AiFillCheckCircle} from "react-icons/ai"
 import './dnd-styles.css'
 
 const Dnd = () => {
-    const [files,setFiles]=useState(null);
-    const [extracted,setExtracted]=useState(false)
-    const [animate,setAnimate]=useState(false)
+    const [files,setFiles]=useState();
+    const [extracted,setExtracted]=useState(false);
+    const [animate,setAnimate]=useState(false);
+    const [emptyMeassage,setEmptyMessage]=useState(false);
     const handleDragOver = (e) => {
         e.preventDefault();
 
@@ -13,22 +14,25 @@ const Dnd = () => {
     
     const handleDrop =(e) =>{
         setAnimate(true)
+        setFiles(false)
         setTimeout(()=>{
             setAnimate(false)
-            
         },1000)
         e.preventDefault();
         setFiles(e.dataTransfer.files[0].name);
-    }
+        setExtracted(false)
+        setEmptyMessage(false)
+    }   
 
 
 
 
     const handleClick = ()=>{
         if(files){
-            setExtracted(()=>{
-                return !extracted
-            })
+            setExtracted(true)
+        }
+        else{
+            setEmptyMessage(true)
         }
     }
 
@@ -49,7 +53,13 @@ const Dnd = () => {
             <div className='uploaded'>
                 {files}
             </div>}
-            <button className='extract-button' onClick={handleClick}> {!extracted?<p>Extract color palette</p>:<span>Extracted Successfully <AiFillCheckCircle className='check-icon'/></span>}</button>
+            {emptyMeassage&& 
+            <div className='uploaded'>
+                No Files Uploaded
+            </div>}
+            <div className='button-container'>
+                <button className='extract-button' onClick={handleClick}> {!extracted?<p>Extract color palette</p>:<span>Extracted Successfully <AiFillCheckCircle className='check-icon'/></span>}</button>
+            </div >
             </div>
         </div>
     </>
