74ded42405006c622a80a14d199b3ff1211162dd
Mustafa Elgmal <mostafaelgmal36@gmail.com>
2023-03-13 01:29:54 +0200

    Add realtime data from backend

--- src/pages/CreateDesign.jsx
+++ src/pages/CreateDesign.jsx
@@ -1,12 +1,30 @@
 import React,{useEffect} from 'react'
+import { useState } from 'react'
 import HeaderOfDesign from '../component/DesignerMode/HeaderOfDesign'
+import { getCategories, getComponents, getComponentsTrending } from '../utils/apis'
 const CreateDesign = () => {
+  const [categories,setCategories]=useState([])
+  const [components,setComponents]=useState([])
+  const [trendingComponents,setTrendingComponents]=useState([])
+  const getAllCategories=async()=>{
+    await getCategories(setCategories)
+  }
+  const getAllComponents=async()=>{
+    await getComponents(setComponents)
+  }
+  const getTrendingAllComponents=async()=>{
+    await getComponentsTrending(setTrendingComponents)
+
+  }
   useEffect(() => {
     window.scrollTo(0, 0)
+    getAllCategories()
+    getAllComponents()
+    getTrendingAllComponents()
   }, [])
   return (
     <div className="min-h-screen">
-     <HeaderOfDesign className="absolute"/>
+     <HeaderOfDesign className="absolute" categories={categories} components={components} trendingComponents={trendingComponents}/>
     </div>
   )
 }
